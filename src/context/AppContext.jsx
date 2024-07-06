65296c08d7c259425e34bdc94396b4fab23a4691
AmirBecovicc <amirbecovic21@gmail.com>
2024-07-06 18:48:49 +0200

    Update site

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,18 @@
+// import { createContext, useState } from "react";
+
+// const AppContext = createContext();
+
+// function ContextProvider({ children }) {
+//   const [loggedInUser, setLoggedInUser] = useState(null);
+//   const [favoriteHotels, setFavoriteHotels] = useState([]);
+
+//   const values = {
+//     loggedInUser,
+//     favoriteHotels,
+//     setLoggedInUser,
+//     setFavoriteHotels,
+//   };
+//   return <AppContext.Provider value={values}>{children}</AppContext.Provider>;
+// }
+
+// export { AppContext, ContextProvider };
